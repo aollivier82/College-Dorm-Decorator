77c35325d5c08ea07665d22bec24b5af364f43f9
Alix Ollivier <aollivier82@gmail.com>
2023-10-17 17:24:52 -0400

    Update PlaceOnTouch.cs
    This was originally supposed to be a poster-placing app. Unfortunately, AR in it's current form doesn't recognize walls very well, and vertical planes are difficult to work with. In an effort to pivot, it's now a plant-placing app. Place plants around your apartment!
    
    Today's work was to fix the build and allow for the placement of plants.
    
    I can now tap the screen and have a model show up.
    
    Next, I need to make it so that it's possible for the user to rotate plants, then add functionality to change which plant the user wants to place. Finally, I'll add animations and sound effects and publish my first AR app!

--- College Dorm Decorator/Assets/PlaceOnTouch.cs
+++ College Dorm Decorator/Assets/PlaceOnTouch.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -7,6 +8,7 @@ using UnityEngine.XR.ARSubsystems;
 public class PlaceOnTouch : MonoBehaviour
 {
     public GameObject objectToPlacePrefab;
+    public Text text;
 
     private ARRaycastManager _arRaycastManager;
     private GameObject _spawnedObject;
@@ -25,28 +27,33 @@ public class PlaceOnTouch : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                if (_arRaycastManager.Raycast(touch.position, _hits, TrackableType.PlaneWithinPolygon))
+                if (_arRaycastManager.Raycast(touch.position, _hits, TrackableType.AllTypes))
                 {
                     Pose hitPose = _hits[0].pose;
 
                     // The forward direction for the poster should be opposite to the wall's normal (hitPose.up)
-                    Vector3 posterForward = -hitPose.up;
+                    //Vector3 posterForward = -hitPose.up;
 
                     // The upward direction for the poster should be aligned with global up to keep it upright
-                    Vector3 posterUp = Vector3.up;
+                    //Vector3 posterUp = Vector3.up;
 
                     // Create a rotation that makes the poster parallel to the wall
-                    Quaternion rotationParallelToWall = Quaternion.LookRotation(posterForward, posterUp);
+                    //Quaternion rotationParallelToWall = Quaternion.LookRotation(posterForward, posterUp);
 
+                    Instantiate(objectToPlacePrefab, hitPose.position, hitPose.rotation);
+                    //text.text = "Spawned";
+
+                    /*
                     if (_spawnedObject == null)
                     {
-                        _spawnedObject = Instantiate(objectToPlacePrefab, hitPose.position, rotationParallelToWall);
+                        _spawnedObject = Instantiate(objectToPlacePrefab, hitPose.position, hitPose.rotation);
+                        text.text = "Spawned";
                     }
                     else
                     {
                         _spawnedObject.transform.position = hitPose.position;
-                        _spawnedObject.transform.rotation = rotationParallelToWall;
-                    }
+                        _spawnedObject.transform.rotation = hitPose.rotation;
+                    }*/
                 }
             }
         }
